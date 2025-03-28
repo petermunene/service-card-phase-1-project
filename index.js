4bb4fd79a0abc9e11cdebcf6a08cb25ea37e1579
petermunene <peter.munene@student.moringaschool.com>
2025-03-28 10:08:32 +0300

    s

--- index.js
+++ index.js
@@ -101,7 +101,7 @@ function displayBar() {
                         service.clients.push(newClient)
                         
 
-                        fetch(`https://my-app-backend-hvge.onrender.com/api/${category.id}`, {
+                        fetch(`https://my-app-backend-hvge.onrender.com/api/services/${category.id}`, {
                             method: "PATCH",
                             headers: {
                                 "Content-Type": "application/json"
